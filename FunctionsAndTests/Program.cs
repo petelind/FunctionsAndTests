bf0f990356b1e5160dee0dc44be4b464e359d846
petelind <d.petelin@icloud.com>
2016-12-07 16:50:28 +0300

    Add project files.

---
+++ FunctionsAndTests/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsAndTests
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /// В ЭТОМ КОДЕ ЕСТЬ КРАЙНЕ НЕПРИЯТНЫЕ ОШИБКИ!!!
+            /// Подумай, а) что за ошибки здесь есть и б) как их исправить?
+            /// TODO: What are the errors in design & how to fix them?
+            
+            // нам понадобится переменная для ширины двора, и она может быть дробной
+            double width;
+            // нам понадобится переменная для длины двора, и она может быть дробной
+            double length;
+            // и денежная переменная для цены
+            decimal finalPrice;
+            // и строка, в которую мы будем читать пользовательский ввод
+            string userInput = "";
+
+            // итак, калькулятор работает пока пользователь не ввел 00
+            while (userInput != "00")
+            {
+                // Информируем пользователя, что он должен ввести ширину
+                Console.WriteLine("Enter width of the yard: ");
+                // затем читаем ширину в строковую переменную
+                userInput = Console.ReadLine();
+                // а теперь конвертируем строку в дробь
+                width = Convert.ToDouble(userInput);
+
+                // Информируем пользователя, что он должен ввести ширину
+                Console.WriteLine("Enter length of the yard: ");
+                // затем читаем ширину в строковую переменную
+                userInput = Console.ReadLine();
+                // а теперь конвертируем строку в дробь
+                length = Convert.ToDouble(userInput);
+
+                // Теперь мы вызываем функцию EstimateOrder (оценить заказ) и...
+                // сохраняем результат в нашу переменную:
+                finalPrice = PriceCalculator.EstimateOrder(width, length);
+                // у нас есть цена и мы выводим ее на экран
+                Console.WriteLine("Final price for your order: " + finalPrice + "$\n\n\n" ); 
+
+            }
+        }
+
+    }
+}
---
+++ FunctionsAndTestsTests/PriceCalculatorTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FunctionsAndTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsAndTests.Tests
+{
+    [TestClass()]
+    public class PriceCalculatorTests
+    {
+        [TestMethod()]
+        public void EstimateOrderTestWithCorrectNumbers()
+        /// Этот тест проверяет, считает ли функция правильно для стандартного случая - без скидок и надбавок
+        {
+            // assign
+            double width = 50;
+            double length = 7.5;
+            decimal expected = 7.5M * 50 * 5;
+            // act
+            decimal actual = PriceCalculator.EstimateOrder(width, length);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void EstimateOrderWithWrongNumbers()
+        /// Этот тест проверяет, вернем ли мы -1 получив 0 или отрицательное число на входе
+        {
+            // assign
+            double width = 10;
+            double length = -10;
+            decimal expected = -1;
+            // act
+            decimal actual = PriceCalculator.EstimateOrder(width, length);
+            // assert 
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EstimateOrderWithPriceUptick ()
+        /// Этот тест проверяет, делаем ли мы надбавку в 20% на заказах меньше 1000
+        {
+            // assign
+            double width = 10;
+            double length = 10;
+            decimal expected = Convert.ToDecimal(width * length * 5 * 1.2);
+
+            // act
+            decimal actual = PriceCalculator.EstimateOrder(width, length);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EstimateOrderWithDiscount()
+        /// а этот тест проверяет, делаем ли мы скидку для заказов больше 2500
+        {
+            // assign
+            double width = 1000;
+            double length = 1000;
+            decimal expected = Convert.ToDecimal(width * length * 5 * 0.95);
+
+            // act
+            decimal actual = PriceCalculator.EstimateOrder(width, length);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
