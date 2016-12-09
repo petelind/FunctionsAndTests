902e5128a4f259c75ffd8c6de634a5c9687c9768
petelind <d.petelin@icloud.com>
2016-12-09 19:49:36 +0300

    Сделан полный текст проверок использую свежеизученные конструкции &&

--- FunctionsAndTests/Program.cs
+++ FunctionsAndTests/Program.cs
@@ -33,13 +33,13 @@ namespace FunctionsAndTests
                 userInput = Console.ReadLine();
                 // а теперь конвертируем строку в дробь
                 // 
-                if (double.TryParse(userInput, out width))
+                if (double.TryParse(userInput, out width) && width > 0) // читается "получилось сделать цифру и она больше 0"...
                 {
-                    width = Convert.ToDouble(userInput);
+                    width = Convert.ToDouble(userInput); // строго говоря, TryParse уже произвел результат, но так наверное понятнее? :)
                 }
                 else
                 {
-                    Console.WriteLine("Incorrect input, only numbers are allowed! Starting over...\n\n");
+                    Console.WriteLine("Incorrect input, only positive numbers are allowed! Starting over...\n\n");
                     continue;
                 }
 
@@ -49,19 +49,28 @@ namespace FunctionsAndTests
                 userInput = Console.ReadLine();
 
                 // а теперь конвертируем строку в дробь
-                if (double.TryParse(userInput, out length))
+                if (double.TryParse(userInput, out length) && length > 0)
                 {
                     length = Convert.ToDouble(userInput);
                 }
                 else
                 {
-                    Console.WriteLine("Incorrect input, only numbers are allowed! Starting over...\n\n");
+                    Console.WriteLine("Incorrect input, only positive numbers are allowed! Starting over...\n\n");
                     continue;
                 }
 
                 // Теперь мы вызываем функцию EstimateOrder (оценить заказ) и...
                 // сохраняем результат в нашу переменную:
                 finalPrice = PriceCalculator.EstimateOrder(width, length);
+
+                // если вдруг мы таки получили -1, что сейчас невозможно, но вдруг?
+                if (finalPrice <= 0)
+                {
+                    Console.WriteLine("Something went terribly wrong, try to start over!");
+                    continue;
+                }
+
+
                 // у нас есть цена и мы выводим ее на экран
                 Console.WriteLine("Final price for your order: " + finalPrice + "$\n\n\n" ); 
 
